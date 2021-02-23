b9e486e39119aa116e86a1876990c58eccf62ea7
Mike Campbell <mike.campbell1967@gmail.com>
2021-02-23 10:31:23 -0500

    added campus administrator access

--- university-post-types.php
+++ university-post-types.php
@@ -4,6 +4,8 @@ function university_post_types() {
 
   // Campus Post Type
   register_post_type('campus', array(
+    'capability_type' => 'campus',
+    'map_meta_cap' => true, 
     'show_in_rest' => true,
     'supports' => array('title', 'editor', 'excerpt'),
     'rewrite' => array('slug' => 'campuses'),
